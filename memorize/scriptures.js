fad0653c53ac637bca33603efa0b283f58183343
baird6 <bai16026@byui.edu>
2017-07-14 17:03:16 -0600

    lots

--- memorize/scriptures.js
+++ memorize/scriptures.js
@@ -1,3 +1,4 @@
+var url = "/memorize/scriptures.json"
 function getJSON(url) {
     return fetch(url)
         .then(function (response) {
@@ -7,6 +8,50 @@ function getJSON(url) {
         console.log(error);
     });
 }
-var url = "/memorize.scriptures.json"
-var jason = getJSON(url);
-console.log(jason);
+
+function loader(page) {
+getJSON(url).then(function (data) {
+    console.log(data);
+    if (page == 'dad') {
+        var info = data.Dad;
+
+        document.getElementById('stixQuote').innerHTML = data.intro[3].text; document.getElementById('quoteAttribution').innerHTML = "- " + data.intro[3].reference;
+    }
+    else if (page == 'hannah') {
+        var info = data.Hannah;
+
+        document.getElementById('stixQuote').innerHTML = data.intro[4].text; document.getElementById('quoteAttribution').innerHTML = "- " + data.intro[4].reference;
+    }
+    else if (page == 'mattie') {
+        var info = data.Mattie;
+
+        var quote = document.getElementById('stixQuote');
+        quote.innerHTML = data.intro[2].text;
+        var attribution = document.getElementById('quoteAttribution');
+        attribution.innerHTML = "- " + data.intro[2].reference;
+
+        quote.style.fontSize = "smaller";
+        attribution.style.fontSize = "smaller";
+
+    }
+    else if (page == 'caleb') {
+        var info = data.Caleb;
+
+        document.getElementById('stixQuote').innerHTML = data.intro[0].text; document.getElementById('quoteAttribution').innerHTML = "- " + data.intro[0].reference;
+    }
+    else {
+        document.getElementById('stixQuote').innerHTML = data.intro[1].text; document.getElementById('quoteAttribution').innerHTML = "- " + data.intro[1].reference;
+    }
+
+    //displaying scriptures
+    for (var i=1; i <= 10; i++) {
+        document.getElementById('reference' + i).innerHTML = info[i-1].reference;
+        document.getElementById('text' + i).innerHTML = info[i-1].text;
+
+    }
+
+
+
+
+})
+}
